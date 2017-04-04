12a52dd1adf170a586830de6d9d41a63e2eecfe6
Arin Arjani <arinarjani@gmail.com>
2017-04-03 17:19:44 -0700

    now on part 4 of the Django tutorial

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# these are going to be passed to the request param in polls.views
 urlpatterns = [
 	url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,18 @@ from django.conf.urls import url
 
 from . import views
 
+# namespace
+app_name = 'polls'
+
+# these are going to be passed to the question_id param in polls.views
+# except for urlpatterns[0], that is simply the request param
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	# ex: /polls/
+	url(r'^$', views.index, name='index'),
+	# ex: /polls/5/
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
+	# ex: /polls/5/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = 'results'),
+	# ex: /polls/5/vote/
+	url(r'(?P<question_id>[0-9]+)/vote/$', views.vote, name = 'vote')
 ]
